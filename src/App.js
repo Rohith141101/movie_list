51c46d302d3c0a6afbc1abf3092dc698bdc31929
Rohith <73777100+Rohith141101@users.noreply.github.com>
2023-10-15 14:56:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import Search from "./components/Search";
 import Movies from "./components/Movies";
+import './index.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
