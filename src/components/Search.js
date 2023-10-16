18e2b605f52785d905a6dbf2ccc80a939f6fdc5a
Rohith <73777100+Rohith141101@users.noreply.github.com>
2023-10-16 12:42:42 +0530

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-const Search = props => (
-  <form className="search" onInput={event => props.onInput(event.target.value)}>
-    <input type="search" value={props.query} placeholder={props.placeholder} />
-  </form>
-);
+
+class Search extends Component {
+  render() {
+    return (
+      <form className="search" onInput={event => this.props.onInput(event.target.value)}>
+        <input type="search" value={this.props.query} placeholder={this.props.placeholder} />
+      </form>
+    );
+  }
+}
 
 Search.propTypes = {
-  query      : PropTypes.string.isRequired,
-  onInput    : PropTypes.func.isRequired,
-  placeholder: PropTypes.string
+  query: PropTypes.string.isRequired,
+  onInput: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
 };
+
 export default Search;
