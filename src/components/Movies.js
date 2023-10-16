a44e9291e672600c193ab110b2ff0bf387e67bd6
Rohith <73777100+Rohith141101@users.noreply.github.com>
2023-10-16 12:41:55 +0530

    Update Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-
 import Movie from "./Movie";
-const Movies = props => (
-    <ul className="movies">
-      {props.movies.map(movie => (
-        <li key={movie.id}>
-          <Movie {...movie} />
-        </li>
-      ))}
-    </ul>
-  );
-  
-  Movies.propTypes = {
-    movies:PropTypes.arrayOf(PropTypes.object)
-  };
-  export default Movies;
+
+class Movies extends Component {
+  render() {
+    return (
+      <ul className="movies">
+        {this.props.movies.map(movie => (
+          <li key={movie.id}>
+            <Movie {...movie} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+Movies.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default Movies;
